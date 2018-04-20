6d429a2dcfe0f9e7c0c58918db81763685329cdc
gaochongyang <gaochongyang@enmonster.com>
2018-04-20 12:02:23 +0800

    创建工程

---
+++ GCYRacTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCYRacTest
+//
+//  Created by gaochongyang on 2018/4/20.
+//  Copyright © 2018年 gaochongyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
