617554a5ac74384055ce9cfb099d1cfa1c67a675
gaochongyang <gaochongyang@enmonster.com>
2018-04-23 15:52:18 +0800

    初探RAC

---
+++ GCYRacTest/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  GCYRacTest
+//
+//  Created by gaochongyang on 2018/4/20.
+//  Copyright © 2018年 gaochongyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ GCYRacTest/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  GCYRacTest
+//
+//  Created by gaochongyang on 2018/4/20.
+//  Copyright © 2018年 gaochongyang. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
